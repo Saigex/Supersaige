fd03e216235d0523e0333eec0caa9b1518ce811b
Saigex <iamsupersaige@gmail.com>
2025-06-23 17:02:24 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,18 +19,6 @@
       $("#preloder").fadeOut("slow");
     }, 5000); // 5 seconds max wait
 
-        /*------------------
-            Portfolio filter
-        --------------------*/
-        $('.portfolio__filter li').on('click', function () {
-            $('.portfolio__filter li').removeClass('active');
-            $(this).addClass('active');
-        });
-        if ($('.portfolio__gallery').length > 0) {
-            var containerEl = document.querySelector('.portfolio__gallery');
-            var mixer = mixitup(containerEl);
-        }
-    });
 
     /*------------------
         Background Set
